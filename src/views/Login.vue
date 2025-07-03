449329f0a3c6d8337f79c4b9d039dfb6367f87b3
kevin <kevin@rock.et>
2025-07-03 11:14:54 +0700

    Update validate component login and register

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,12 +7,12 @@
       </div>
 
       <div class="mt-6">
-        <form class="space-y-6" action="#" method="POST" @submit="checkValidateLogin">
+        <form class="space-y-6" action="#" method="POST" @submit.prevent="onSubmit">
           <div>
             <label for="email" class="block text-sm/6 font-medium text-gray-900">Email <span class="text-sm text-red-500">*</span></label>
             <div class="mt-2">
               <input v-model="email" type="email" name="email" id="email" autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-              <p v-if="errorEmail" class="text-xs text-red-500 mt-1">{{ errorEmail }}</p>
+              <p class="text-xs text-red-500 mt-1">{{ errors.email }}</p>
             </div>
           </div>
 
@@ -20,7 +20,7 @@
             <label for="password" class="block text-sm/6 font-medium text-gray-900">Mật khẩu <span class="text-sm text-red-500">*</span></label>
             <div class="mt-2">
               <input type="password" name="password" id="password" autocomplete="password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" v-model="password" />
-              <p v-if="errorPassword" class="text-xs text-red-500 mt-1">{{ errorPassword }}</p>
+              <p class="text-xs text-red-500 mt-1">{{ errors.password }}</p>
             </div>
           </div>
 
@@ -40,10 +40,6 @@
               <router-link to="/register" class="font-semibold text-indigo-600 hover:text-indigo-500">Bạn chưa có tài khoản?</router-link>
             </div>
           </div>
-
-          <div v-if="errorApi" class="block w-full bg-red-100 rounded-md px-3 py-1.5 text-sm/6 font-semibold text-red-500 shadow-xs">
-            {{ errorApi }}
-          </div>
         </form>
       </div>
     </div>
@@ -55,68 +51,44 @@
   import { useAuthStore } from '../stores/auth';
   import axios from 'axios';
   import { ref } from 'vue';
+  import { ElMessage } from 'element-plus';
+  import * as yup from 'yup';
+  import { useForm, useField } from 'vee-validate';
 
   const router = useRouter();
   const auth = useAuthStore();
-  const email = ref('');
-  const password = ref('');
 
-  // errors state
-  const errorEmail = ref('');
-  const errorPassword = ref('');
-  const errorApi = ref();
+  const schema = yup.object({
+    email: yup.string().required('Email là bắt buộc').email('Email không hợp lệ'),
+    password: yup.string().required('Mật khẩu là bắt buộc').min(6, 'Mật khẩu ít nhất 6 ký tự')
+  });
+  const { handleSubmit, errors } = useForm({ validationSchema: schema });
+  const { value: email } = useField('email');
+  const { value: password } = useField('password');
 
-  function validateEmailError(value) {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(value);
-  }
-  
-  function checkValidateLogin(event) {
-    event.preventDefault();
-    // Kiểm tra tính hợp lệ form
-    let isValid = true;
-    
-    // reset errors
-    errorEmail.value = '';
-    errorPassword.value = '';
-
-    if (!email.value) {
-      errorEmail.value = 'Vui lòng nhập email';
-      isValid = false;
-    } else if (!validateEmailError(email.value)) {
-      errorEmail.value = 'Email không hợp lệ';
-      isValid = false;
-    }
-
-    if (!password.value) {
-      errorPassword.value = 'Vui lòng nhập mật khẩu';
-      isValid = false;
-    }
-
-    // Pass form
-    if (isValid) {
-      handleLogin();
-    }
-  }
-
-  async function handleLogin() {
+  const onSubmit = handleSubmit(async (values) => {
     try {
-      errorApi.value = '';
-
-      const response = await axios.post('auth/login', {
-        email: email.value,
-        password: password.value
-      });
+      const response = await axios.post('auth/login', values);
 
       auth.login({
         user: response.data.data.user,
         token: response.data.data.tokens.accessToken
       });
+
+      ElMessage({
+        message: response.data.message || 'Xảy ra lỗi',
+        type: 'success',
+        grouping: true
+      });
       
       router.push('/');
     } catch (error) {
       console.error('Login failed: ', error);
-      errorApi.value = error.response.data.message;
+      ElMessage({
+        message: error.response.data.message || 'Xảy ra lỗi',
+        type: 'error',
+        grouping: true
+      });
     }
-  }
+  });
 </script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -7,46 +7,48 @@
       </div>
 
       <div class="mt-6">
-        <form class="space-y-4" action="#" method="POST" @submit="checkValidateRegister">
+        <form class="space-y-4" action="#" method="POST" @submit.prevent="onSubmit">
           <div class="flex items-center gap-2">
             <div class="w-1/2">
-              <label for="email" class="block text-sm/6 font-medium text-gray-900">Email</label>
+              <label for="email" class="block text-sm/6 font-medium text-gray-900">Email <span class="text-sm text-red-500">*</span></label>
               <div class="mt-1">
-                <input v-model="email" type="email" name="email" id="email" autocomplete="email" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                <input v-model="email" type="email" name="email" id="email" autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                <p class="text-red-500 text-xs mt-1">{{ errors.email }}</p>
               </div>
             </div>
 
             <div class="w-1/2">
-              <div class="flex items-center justify-between">
-                <label for="password" class="block text-sm/6 font-medium text-gray-900">Mật khẩu</label>
-              </div>
+              <label for="password" class="block text-sm/6 font-medium text-gray-900">Mật khẩu <span class="text-sm text-red-500">*</span></label>
               <div class="mt-1">
-                <input type="password" name="password" id="password" autocomplete="current-password" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" v-model="password" />
+                <input type="password" name="password" id="password" autocomplete="current-password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" v-model="password" />
+                <p class="text-red-500 text-xs mt-1">{{ errors.password }}</p>
               </div>
             </div>
           </div>
 
           <div class="flex items-center gap-2">
             <div class="w-1/2">
-              <label for="username" class="block text-sm/6 font-medium text-gray-900">Tên người dùng</label>
+              <label for="name" class="block text-sm/6 font-medium text-gray-900">Tên người dùng <span class="text-sm text-red-500">*</span></label>
               <div class="mt-1">
-                <input v-model="username" type="text" name="username" id="username" autocomplete="username" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                <input v-model="name" type="text" name="name" id="name" autocomplete="name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                <p class="text-red-500 text-xs mt-1">{{ errors.name }}</p>
               </div>
             </div>
 
+            <div class="w-1/2">
+              <label for="phone" class="block text-sm/6 font-medium text-gray-900">Số điện thoại <span class="text-sm text-red-500">*</span></label>
+              <div class="mt-1">
+                <input v-model="phone" type="text" name="phone" id="phone" autocomplete="phone" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                <p class="text-red-500 text-xs mt-1">{{ errors.phone }}</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex items-center gap-2">
             <div class="w-1/2">
               <label for="dateOfBirth" class="block text-sm/6 font-medium text-gray-900">Năm sinh</label>
               <div class="mt-1">
-                <input v-model="dateOfBirth" type="date" name="dateOfBirth" id="dateOfBirth" autocomplete="email" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-              </div>
-            </div>
-          </div>
-
-          <div class="flex items-center gap-2">
-            <div class="w-1/2">
-              <label for="phone" class="block text-sm/6 font-medium text-gray-900">Số điện thoại</label>
-              <div class="mt-1">
-                <input v-model="phone" type="text" name="number" id="number" autocomplete="number" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                <input v-model="dateOfBirth" type="date" name="dateOfBirth" id="dateOfBirth" autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
               </div>
             </div>
 
@@ -66,16 +68,17 @@
           <div>
             <label for="address" class="block text-sm/6 font-medium text-gray-900">Địa chỉ</label>
             <div class="mt-1">
-              <input v-model="address" type="text" name="address" id="address" autocomplete="address" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+              <input v-model="address" type="text" name="address" id="address" autocomplete="address" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
             </div>
           </div>
 
           <div>
             <button 
-            type="submit" 
-            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            @click="handleRegister"
-            >Đăng ký</button>
+              type="submit" 
+              class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Đăng ký
+            </button>
           </div>
           <div class="text-sm">
             <router-link to="/login" class="font-semibold text-indigo-600 hover:text-indigo-500 flex items-center">
@@ -91,41 +94,54 @@
 
 <script setup>
   import axios from 'axios';
-  import { ref } from 'vue';
   import { useRouter } from 'vue-router';
+  import * as yup from 'yup';
+  import { useForm, useField } from 'vee-validate';
+  import { ElMessage } from 'element-plus'
+
+  // định nghĩa schema cho việc validate
+  const schema = yup.object({
+    email: yup.string().required('Email là bắt buộc').email('Email không hợp lệ'),
+    password: yup.string().required('Mật khẩu là bắt buộc').min(6, 'Mật khẩu phải ít nhất 6 ký tự'),
+    name: yup.string().required('Tên người dùng là bắt buộc'),
+    phone: yup.string().required('Số điện thoại là bắt buộc'),
+    dateOfBirth: yup.date().nullable(),
+    gender: yup.string().nullable(),
+    address: yup.string().nullable()
+  });
+
+  // Khởi tạo form voiws lib vee-validate
+  const { handleSubmit, errors } = useForm({ validationSchema: schema });
+  const { value: email } = useField('email');
+  const { value: password } = useField('password');
+  const { value: name } = useField('name');
+  const { value: phone } = useField('phone');
+  const { value: dateOfBirth } = useField('dateOfBirth');
+  const { value: gender } = useField('gender');
+  const { value: address } = useField('address');
 
   const router = useRouter();
-  const email = ref('');
-  const password = ref('');
-  const username = ref('');
-  const dateOfBirth = ref('');
-  const phone = ref('');
-  const gender = ref('');
-  const address = ref('');
-
-  function checkValidateRegister(event) {
-    event.preventDefault();
-  }
-
-  async function handleRegister() {
+  const onSubmit = handleSubmit(async (values) => {
     try {
-      const data = {
-        name: username.value,
-        email: email.value,
-        password: password.value,
-        role: 'customer',
-        dateOfBirth: dateOfBirth.value,
-        phone: phone.value,
-        gender: gender.value,
-        address: address.value
-      };
+      const response = await axios.post('users/register', {
+        ...values,
+        role: 'customer'
+      });
 
-      const response = await axios.post('users/register', data);
       if (response.status === 201) {
+        ElMessage({
+          message: response.data.message || 'Xảy ra lỗi',
+          type: 'success',
+          grouping: true
+        });
+
         router.push('/login');
       }
     } catch (error) {
-      console.error('Register faild: ', error)
+      console.error('Đăng ký thất bại: ', error);
+      ElMessage({
+        message: error.response.data.message || 'Xảy ra lỗi'
+      });
     }
-  }
+  });
 </script>
